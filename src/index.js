0a65037b24a0701fb9786d669efdc279bc49c0e3
Семененко Надежда <tn@impsa.ru>
2023-02-19 22:28:24 +0300

    add top-content components, start card page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './scss/app.scss';
 
 import App from './App';
+// import { Home } from './pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      {/* <Routes>
+        <Route path="/" element={<App />} />
+      </Routes> */}
+    </Router>
   </React.StrictMode>,
 );
